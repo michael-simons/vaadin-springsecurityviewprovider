0ab19087037e33f0e051e4b221048d751e151611
Michael J. Simons <michael@simons.ac>
2013-03-05 10:40:20 +0100

    Added the scope to this annotation

--- src/main/java/de/enerko/aaw2/app/vaadin/ViewDescription.java
+++ src/main/java/de/enerko/aaw2/app/vaadin/ViewDescription.java
@@ -1,11 +1,14 @@
 package de.enerko.aaw2.app.vaadin;
 
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Component;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Component
+@Scope(SCOPE_PROTOTYPE)
 public @interface ViewDescription {
 	/** The name of the view, also used by the navigator */
 	String name() default "";
