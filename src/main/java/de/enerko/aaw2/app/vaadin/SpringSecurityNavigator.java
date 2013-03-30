f9963bee0bd3add262016cd6f76fecbeb7795f2f
Michael J. Simons <michael@simons.ac>
2013-03-30 13:52:34 +0100

    Implemented caching / more documentation

--- src/main/java/de/enerko/aaw2/app/vaadin/SpringSecurityNavigator.java
+++ src/main/java/de/enerko/aaw2/app/vaadin/SpringSecurityNavigator.java
@@ -1,6 +1,7 @@
 package de.enerko.aaw2.app.vaadin;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,7 +14,6 @@ import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.security.access.expression.ExpressionUtils;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.util.SimpleMethodInvocation;
 
@@ -24,19 +24,14 @@ import com.vaadin.navigator.ViewProvider;
 import com.vaadin.ui.UI;
 
 /**
- * Dieser spezielle Navigator wertet die Annotation
- * {@link ViewDescription} aus.
+ * This is a specialized Navigator that takes care of evaluating {@link ViewDescription} 
+ * especially {@link ViewDescription#requiredPermissions()}. Those expressions are evaluated
+ * within the current Spring Security Context. If they evaluate to false, the view in concern
+ * is not even instantiated.
  * 
- * Falls die Beschreibung {@link ViewDescription#requiredPermissions()}
- * anfordert, werden diese Permissions genauso wie {@link PreAuthorize
- * Ausdrücke im aktuellen Spring Security Context ausgewertet. Fals die
- * Auswertung false ergibt, wird diese View erst gar nicht geladen.
+ * The navigator then uses the SpringViewProvider that finally instantiates all views.
  * 
- * Ansonsten wird ein eigener View Provider genutzt, der die Beans bei
- * Bedarf aus dem Application Context bezieht.
- * 
- * Von diesem Navigator werden keine <code>add*</code> oder <code>remove*</code>
- * Methoden unterstützt.
+ * This navigator doesn't and shouldn't support adding and removing views.
  * 
  * @author Michael J. Simons, 2013-03-04
  */
@@ -44,14 +39,18 @@ public class SpringSecurityNavigator extends Navigator {
 	private static final long serialVersionUID = -7896790310309542217L;
 
 	/**
-	 * View Provider, der Views aus dem Spring Application Context bezieht
+	 * View provider that pulls views from the application context and optionally caches them.
 	 */
 	@Configurable
-	private class SpringViewProvider implements ViewProvider {		
+	private class SpringViewProvider implements ViewProvider {
 		private static final long serialVersionUID = -8555986824827085073L;
+		/** Will be injected through AspectJ upon new and deserialization */
 		@Autowired
 		transient ApplicationContext applicationContext;
-		final Map<String, Class<? extends View>> views = new HashMap<>();		
+		/** The viewname -> view class mapping */
+		final Map<String, Class<? extends View>> views = new HashMap<>();
+		/** Cached instances of views */
+		final Map<String, View> cachedInstances = new HashMap<>();
 
 		@Override
 		public String getViewName(String viewAndParameters) {
@@ -72,9 +71,30 @@ public class SpringSecurityNavigator extends Navigator {
 		}
 
 		@Override
-		public View getView(String viewName) {
-			return applicationContext.getBean(this.views.get(viewName));
+		public View getView(String viewName) {			
+			View rv = null;
+			
+			// Retrieve the implementing class
+			Class<? extends View> clazz = this.views.get(viewName);
+			// Try to find cached instance of caching is enabled and the view is cacheable
+			if(isCachingEnabled() && clazz.getAnnotation(ViewDescription.class).cacheable()) {
+				rv = this.cachedInstances.get(viewName);
+				// retrieve the new instance and cache it if it's not already cached.
+				if(rv == null)
+					this.cachedInstances.put(viewName, rv = applicationContext.getBean(clazz));
+			} else {
+				rv = applicationContext.getBean(clazz);
+			}
+			return rv;
 		}		
+		
+		/**
+		 * Caching should be enabled only in "prod" environment
+		 * @return true if caching is enabled
+		 */
+		boolean isCachingEnabled() {
+			return Arrays.asList(this.applicationContext.getEnvironment().getActiveProfiles()).contains("prod");
+		}
 	}
 
 	@SuppressWarnings("unchecked")
@@ -89,18 +109,29 @@ public class SpringSecurityNavigator extends Navigator {
 		try {
 			final SpringViewProvider springViewProvider = new SpringViewProvider();		
 	
+			// Retrieve the default SecurityExpressionHandler 
 			final MethodSecurityExpressionHandler securityExpressionHandler = applicationContext.getBean(DefaultMethodSecurityExpressionHandler.class);
+			// The method that is protected in the end
 			final Method getViewMethod = SpringViewProvider.class.getMethod("getView", String.class);
+			// A parser to evaluate parse the permissions.
 			final SpelExpressionParser parser = new SpelExpressionParser();
 	
+			// Although beans can be retrieved by annotation they must be retrieved by name
+			// to avoid instanciating them
 			for(String beanName : applicationContext.getBeanDefinitionNames()) {
 				final Class<?> beanClass = applicationContext.getType(beanName);
+				// only work with Views that are described by our specialed Description
 				if (beanClass.isAnnotationPresent(ViewDescription.class) && View.class.isAssignableFrom(beanClass)) {
-					ViewDescription viewDescription = beanClass.getAnnotation(ViewDescription.class);
+					final ViewDescription viewDescription = beanClass.getAnnotation(ViewDescription.class);
+					// requires no special permissions and can be immediatly added
 					if(StringUtils.isBlank(viewDescription.requiredPermissions())) {
 						springViewProvider.views.put(viewDescription.name(), (Class<? extends View>) beanClass);
-					} else {
+					} 
+					// requires permissions
+					else {
+						// this is actually borrowed from the code in org.springframework.security.access.prepost.PreAuthorize
 						final EvaluationContext evaluationContext = securityExpressionHandler.createEvaluationContext(authentication, new SimpleMethodInvocation(springViewProvider, getViewMethod, viewDescription.name()));
+						// only add the view to my provider if the permissions evaluate to true
 						if(ExpressionUtils.evaluateAsBoolean(parser.parseExpression(viewDescription.requiredPermissions()), evaluationContext))
 							springViewProvider.views.put(viewDescription.name(), (Class<? extends View>) beanClass);							
 					}
--- src/main/java/de/enerko/aaw2/app/vaadin/ViewDescription.java
+++ src/main/java/de/enerko/aaw2/app/vaadin/ViewDescription.java
@@ -11,7 +11,12 @@ import java.lang.annotation.Target;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import com.vaadin.navigator.View;
+
 /**
+ * An annotation that describes a Vaadin {@link View}<br>
+ * A view is a managed Component whose scope is prototype, meaning
+ * you'll always get a fresh instance for every application instance.
  * @author Michael J. Simons, 2013-03-04
  */
 @Target({ElementType.TYPE})
@@ -20,11 +25,16 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope(SCOPE_PROTOTYPE)
 public @interface ViewDescription {
-	/** The name of the view, also used by the navigator */
+	/** The name of the view, also used by the navigator, can be a complete path like /foo/bar/baz */
 	String name() default "";
 
-    /**
-     * @return the Spring-EL expression to be evaluated before invoking the protected method
-     */
+	/**
+	 * @return the Spring-EL expression to be evaluated before the view described here is finally added to the navigator.
+	 */
 	String requiredPermissions() default "";
+
+	/**
+	 * @return Can this view be cached if caching is available?
+	 */
+	boolean cacheable() default false;
 }
